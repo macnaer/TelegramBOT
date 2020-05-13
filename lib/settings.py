5a263b00091cd7a8b9b2109d657dc18116e048ef
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-05-13 14:26:16 +0300

    Added ENV vars

--- lib/settings.py
+++ lib/settings.py
@@ -1,4 +1,7 @@
-host = "52.255.162.235"
-user = "wordpress"
-passwd = "wordpress"
-token = "1221686129:AAE5LMMmpaOJUkf-JR8TTiXvSMJDM1bxrmM"
+import os
+host = os.environ['DB_HOST']
+user = os.environ['DB_USER']
+# passwd = ""
+passwd = os.environ['DB_PASSWORD']
+#token = "1221686129:AAE5LMMmpaOJUkf-JR8TTiXvSMJDM1bxrmM"
+token = os.environ['TELEGRAM_TOKEN']
